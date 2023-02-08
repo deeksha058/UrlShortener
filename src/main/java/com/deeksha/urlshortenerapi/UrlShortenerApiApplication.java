90ec29a3e034cec61d8d6e68c3c4d8b9f4815e89
deeksha058 <deekshajindal058@gmail.com>
2023-02-08 22:26:17 +0530

    Initial commit

---
+++ src/main/java/com/deeksha/urlshortenerapi/UrlShortenerApiApplication.java
@@ -0,0 +1,14 @@
+package com.deeksha.urlshortenerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class UrlShortenerApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApiApplication.class, args);
+        System.out.println("Program Run Successfully!");
+    }
+}
---
+++ src/main/java/com/deeksha/urlshortenerapi/entity/Url.java
@@ -0,0 +1,59 @@
+package com.deeksha.urlshortenerapi.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "url")
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String longUrl;
+
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    @Column(nullable = false)
+    private Date createdDate;
+
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    private Date expiresDate;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getExpiresDate() {
+        return expiresDate;
+    }
+
+    public void setExpiresDate(Date expireDate) {
+        this.expiresDate = expireDate;
+    }
+}
